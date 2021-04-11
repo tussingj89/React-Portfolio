dffd0cb0c030736422d2622ecea92162ce21a8e9
tussingj89 <tussing40@gmail.com>
2021-04-11 15:58:55 -0400

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from "./components/Footer"
 import Navigation from "./components/Nav"
 import Wrapper from "./components/Wrapper"
