f5f8ce0106bd2c9b6b2b43f31e0e04bf0c4df34b
tussingj89 <tussing40@gmail.com>
2021-04-11 16:02:26 -0400

    update

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -10,10 +10,10 @@ function Navigation() {
   <Navbar.Toggle aria-controls="basic-navbar-nav" />
   <Navbar.Collapse id="basic-navbar-nav">
     <Nav className="ml-auto">
-      <Nav.Link href="/About">About</Nav.Link>
-      <Nav.Link href="/Skills">Skills</Nav.Link>
+      <Nav.Link href="/#/About">About</Nav.Link>
+      <Nav.Link href="/#/Skills">Skills</Nav.Link>
       {/* <Nav.Link href="/Contact">Contact</Nav.Link> */}
-      <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
+      <Nav.Link href="/#/Portfolio">Portfolio</Nav.Link>
     </Nav>
   </Navbar.Collapse>
 </Navbar>
